9179cf4ffab6cbf39da6a5819b5ae19eb6515486
Andrey Kuzmin <andrey.kuzmin@soundcloud.com>
2018-08-22 23:29:51 +0200

    Try with less uglyfying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,8 @@ mkdir -p dest/assets
 elm make src/Main.elm --optimize --output dest/elm-temp.js
 
 # minify with uglifyjs
-uglifyjs dest/elm-temp.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=dest/elm.js
+# uglifyjs dest/elm-temp.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=dest/elm.js
+uglifyjs dest/elm-temp.js --compress --mangle --output=dest/elm.js
 
 rm dest/elm-temp.js
 
