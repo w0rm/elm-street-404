6c79e9ef13b51eafafac92ee9c6c0787d7908da9
Andrey Kuzmin <andrey.kuzmin@soundcloud.com>
2018-08-22 23:05:37 +0200

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,11 @@ rm -rf dest || exit 0;
 mkdir -p dest/assets
 
 # compile JS using Elm
-elm make src/Main.elm --yes --output dest/elm-temp.js
-uglifyjs dest/elm-temp.js -c warnings=false -m --screw-ie8 -o dest/elm.js
+elm make src/Main.elm --optimize --output dest/elm-temp.js
+
+# minify with uglifyjs
+uglifyjs dest/elm-temp.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=dest/elm.js
+
 rm dest/elm-temp.js
 
 # copy the images and html
